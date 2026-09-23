26ff089e765b29206e6cb876362a4f0ff8666220
HoangnxSoftware <hoangnx0107@gmail.com>
2026-09-23 22:47:36 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/HR-Salary/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
